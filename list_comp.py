dded450e80a92474485e6c4e04374a6432915ce1
Vladimir Hristov <vhristov97@gmail.com>
2021-11-20 10:40:27 +0100

    Razbijena funkcija na vise podfunkcija

--- list_comp.py
+++ list_comp.py
@@ -1,16 +1,20 @@
 import math
 
+def nova_lista(n):
+    return [i for i in range(1, n)]
+
+def lista_korena(l):
+    return [x for x in l if math.sqrt(x).is_integer()]
+
 def main():
     n = 50
     lista = []
-    konacnaLista = []
     
-    for i in range(1, n):
-        lista.append(i)
+    lista = nova_lista(n)
     
-    konacnaLista = [x for x in lista if math.sqrt(x).is_integer()]
+    konacna_lista = lista_korena(lista)
 
-    print(konacnaLista)
+    print(konacna_lista)
 
 if __name__ == "__main__":
     main()
